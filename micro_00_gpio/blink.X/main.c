db5dda8734735913cc8a4708e7fbce7a426156f1
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2023-04-23 15:58:52 -0500

    blink 1

---
+++ micro_00_gpio/blink.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: jmott
+ *
+ * Created on 23 de abril de 2023, 03:55 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Main
+ */
+int main(void) 
+{
+    return (EXIT_SUCCESS);
+}
+
