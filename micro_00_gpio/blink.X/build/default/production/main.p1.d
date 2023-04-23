cc414166e358a0ea04bcb7ac844b3204b8c6bef9
Julio Francisco Motta Claudio <59737000+FranciscoMotta@users.noreply.github.com>
2023-04-23 16:18:59 -0500

    hello world

--- micro_00_gpio/blink.X/build/default/production/main.p1.d
+++ micro_00_gpio/blink.X/build/default/production/main.p1.d
@@ -1,2 +1,14 @@
-build/default/production/main.p1:  \
-main.c 
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/language_support.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/errata.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/pic18.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/__size_t.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/pic18_chip_select.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/xc.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/proc/pic18f57q43.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/htc.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/__null.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/__at.h  \
+D:/Program\ Files/Microchip/MPLABX/v6.05/packs/Microchip/PIC18F-Q_DFP/1.14.237/xc8/pic/include/builtins.h 
--- micro_00_gpio/blink.X/main.c
+++ micro_00_gpio/blink.X/main.c
@@ -5,14 +5,47 @@
  * Created on 23 de abril de 2023, 03:55 PM
  */
 
+#include <xc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+/*
+ * Definicion de macros
+ */
+
+#define User_Led_Sys        3
+#define User_Led_Ansel      ANSELF
+#define User_Led_Tris       TRISF
+#define User_Led_Lat        LATF
+
+/*
+ * Declaracion de funciones
+ */
+
+void GPIO_Init (void);
 
 /*
  * Main
  */
 int main(void) 
 {
+    GPIO_Init();
+    while (true)
+    {
+        // Prendemos el LED
+        User_Led_Lat &= ~(1 << User_Led_Sys); // Mandamos un 0
+    }
     return (EXIT_SUCCESS);
 }
 
+/*
+ * Definicion de funciones
+ */
+
+
+void GPIO_Init (void)
+{
+    User_Led_Ansel &= ~(1 << User_Led_Sys); // Pin digital
+    User_Led_Tris &= ~(1 << User_Led_Sys); // Pin como salida
+}
