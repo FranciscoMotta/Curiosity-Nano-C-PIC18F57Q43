a06ecdc1267cb134887491ae166d27a80917dee9
unknown <Administrador@UTP380696.grupoutp.net>
2023-04-25 19:03:30 -0500

    add bt matrix with driver

---
+++ micro_00_gpio/bt_mat_922.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=bt_mat_922.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/bt_mat_922.X.production.hex
