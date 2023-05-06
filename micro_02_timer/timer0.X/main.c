c9b64a53c9bb3fea8fca8b924bde9ad83f6b12ff
unknown <Administrador@UTP380696.grupoutp.net>
2023-05-06 14:29:27 -0500

    add main timer

--- micro_02_timer/timer0.X/main.c
+++ micro_02_timer/timer0.X/main.c
@@ -5,14 +5,52 @@
  * Created on 6 de mayo de 2023, 01:32 PM
  */
 
+/*
+ * Includes
+ */
+
+#ifndef _XTAL_FREQ
+#define _XTAL_FREQ 16000000UL
+#endif
+
+#include <xc.h>
+#include <pic18.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "system_config.h"
 
 /*
- * 
+ * Macros
  */
-int main(int argc, char** argv) {
 
+/*
+ * Declaracion de funciones
+ */
+
+void Init_Internal_Oscillator (void);
+
+/*
+ * Main
+ */
+int main(void) 
+{
+    Init_Internal_Oscillator();
     return (EXIT_SUCCESS);
 }
 
+/*
+ * Definicion de funciones
+ */
+
+void Init_Internal_Oscillator (void)
+{
+    _clock_hfintosc_params_t my_clock;
+    my_clock.divisor_clock = clock_div_1;
+    my_clock.frecuencia_clock = freq_clk_16MHZ;
+    
+    /* Pasamos los parámetros */
+    FM_Hfintosc_Init(&my_clock);
+}
